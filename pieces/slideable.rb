2eecde741a2c590b28c01f0d025995e911ecb75b
nicolaspiper <nicolaspiper@yahoo.com>
2020-02-29 15:16:10 -0800

    build out empty modules slideable and steppable

---
+++ pieces/slideable.rb
@@ -0,0 +1,28 @@
+module Slideable
+
+    HORIZONTAL_DIRS = []
+    DIAGONAL_DIRS = []
+
+    def horizontal_dirs
+    
+    end
+
+    def diagonal_dirs 
+    
+    end
+
+    def moves
+
+    end
+
+    private 
+
+    def move_dirs
+        raise NotImplementedError
+    end
+
+    def grow_unblocked_moves_in_dir(dx,dy)
+
+    end
+
+end
---
+++ pieces/steppable.rb
@@ -0,0 +1,13 @@
+module Steppable
+
+    def moves
+    
+    end
+
+    private
+
+    def move_diffs
+        raise NotImplementedError
+    end
+
+end
