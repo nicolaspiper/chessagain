e80ad9b1ea3f5c6cc279026f01f6f28e9c011790
nicolaspiper <nicolaspiper@yahoo.com>
2020-02-29 13:47:15 -0800

    modify board class to complete phase 1

--- board.rb
+++ board.rb
@@ -1,7 +1,42 @@
 require_relative 'piece'
+require_relative 'errors'
 
 class Board
+    
     def initialize
-        @board = Array.new(8){Array.new(8)}
+        @rows = Array.new(8){ |row| 
+            Array.new(8) { |col| 
+                Piece.new(nil,nil,[row,col])
+            }
+        }
     end 
+
+    def [](pos)
+        raise InvalidPosition unless valid_pos?(pos)
+        
+        row, col = pos
+        @rows[row][col]
+    end
+    
+    def []=(pos,val)
+        raise InvalidPosition unless valid_pos?(pos)
+        
+        row, col = pos
+        @rows[row][col] = val
+    end
+
+    def empty?(pos)
+        self[pos].empty?
+    end
+
+    def move_piece(turn_color, start_pos, end_pos)
+        piece = self[start_pos]
+        self[start_pos] = nil
+        self[end_pos] = piece
+    end
+
+    def valid_pos?(pos)
+        pos.all? { |coor| coor >= 0 && coor <= 7}
+    end
+
 end
