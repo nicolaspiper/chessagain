1653f04d3227ff75d9d175c75fd4565076f2d530
nicolaspiper <nicolaspiper@yahoo.com>
2020-02-29 14:35:57 -0800

    create empty instance methods of pawn subclass per the UML

---
+++ pieces/pawn.rb
@@ -0,0 +1,30 @@
+require_relative '../peice'
+class Pawn < Piece
+
+    def symbol
+        
+    end
+
+    def move_dirs
+    
+    end
+
+    private
+
+    def at_start_row?(pos)
+    
+    end
+
+    def forward_dir
+    
+    end
+
+    def forward_steps
+    
+    end
+
+    def side_attacks
+        
+    end
+
+end
