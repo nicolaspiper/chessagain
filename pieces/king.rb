bd45645ad521dc326818e03ced8aec8ef60f5f6e
nicolaspiper <nicolaspiper@yahoo.com>
2020-02-29 15:07:10 -0800

    build out empty king and knight classes

---
+++ pieces/king.rb
@@ -0,0 +1,17 @@
+require_relative '../piece'
+require_relative 'steppable'
+
+class King < Piece 
+    include Steppable
+
+    def symbol
+    
+    end
+
+    protected
+
+    def move_diffs
+
+    end
+
+end
---
+++ pieces/knight.rb
@@ -0,0 +1,17 @@
+require_relative '../piece'
+require_relative 'steppable'
+
+class Knight < Piece 
+    include Steppable
+
+    def symbol
+    
+    end
+
+    protected
+
+    def move_diffs
+
+    end
+
+end
