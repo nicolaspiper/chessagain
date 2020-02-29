f6673209d128a145d057853e62fa335e2419c51f
nicolaspiper <nicolaspiper@yahoo.com>
2020-02-29 13:57:44 -0800

    create empty instance methods of piece superclass according to UML

--- piece.rb
+++ piece.rb
@@ -10,10 +10,37 @@ require_relative 'pieces/steppable'
 
 class Piece
 
-    def initialize(color=nil, board=nil, pos)
+    def initialize(color, board, pos)
         @color = color
         @board = board
         @pos = pos
 
     end
+
+    def to_s
+        
+    end
+
+    def empty?
+    
+    end
+
+    def valid_moves
+    
+    end
+
+    def pos=(val)
+    
+    end
+
+    def symbol
+    
+    end
+
+    private
+
+    def move_into_check?(end_pos)
+    
+    end
+
 end
