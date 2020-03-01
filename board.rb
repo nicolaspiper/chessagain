39793009d8a88f9b4873d2a914bd2f62eab23db0
nicolaspiper <nicolaspiper@yahoo.com>
2020-02-29 16:08:18 -0800

    modify board class, more work to be done

--- board.rb
+++ board.rb
@@ -4,6 +4,7 @@ require_relative 'errors'
 class Board
     
     def initialize
+        setup_board
         @rows = Array.new(8){ |row| 
             Array.new(8) { |col| 
                 Piece.new(nil,nil,[row,col])
@@ -37,6 +38,22 @@ class Board
 
     def valid_pos?(pos)
         pos.all? { |coor| coor >= 0 && coor <= 7}
+    end
+
+    def add_piece(piece)
+        raise OccupiedPosition unless self.empty?(piece.pos)
+        self[piece.pos]= piece
+    end
+
+    private
+    
+    def setup_board
+        
+    end
+
+    def place_pawns(color)
+        i = color == :white ? 6 : 1
+        (0...8).each {|j| Pawn.new(color,self,[i,j])}
     end
 
 end
